b4678481b0540abe6b7c146f4817a7dc7b7a9543
Climax Mba <climaxium150@gmail.com>
2023-02-24 07:51:49 +0100

    Solve 06_leapYears/

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(yr) {
+    if ((yr % 100 == 0) && (yr % 400 != 0)) {
+        return false;
+    } else if ((yr % 4 == 0) || (yr % 400 == 0)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
