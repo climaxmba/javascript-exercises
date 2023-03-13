52b4d0a03c0fabc88d0856dfe198929a527ac782
Climax Mba <climaxium150@gmail.com>
2023-03-13 11:11:25 +0100

    Solve 11_getTheTitles/

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        newArr.push(array[i].title);
+    }
+    return newArr;
 };
 
 // Do not edit below this line
