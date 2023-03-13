12b666c54feac2a297f1ec2077dfd087595675b9
Climax Mba <climaxium150@gmail.com>
2023-03-13 09:06:36 +0100

    Solve 09_palindromes/

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let tempArr = string.toLowerCase().split('');
+    let str = [];
+    let punctuations = '.!, ';
+    for (let i = 0; i < tempArr.length; i++) {
+        if (!punctuations.includes(tempArr[i])) {
+            str.push(tempArr[i]);
+        }
+    }
+    return str.join() == str.reverse().join();
 };
 
 // Do not edit below this line
