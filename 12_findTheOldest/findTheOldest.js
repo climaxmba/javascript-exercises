edc4cfd6c30ec567a1ccf2b4271ff1fd567635d7
Climax Mba <climaxium150@gmail.com>
2023-03-14 05:45:15 +0100

    Solve 12_findTheOldest/

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    let currYear = Number(Date().slice(11, -46));
+    let oldest = 0;
 
+    for (let i = 0; i < obj.length; i++) {
+        if (!(obj[i].yearOfDeath)) {
+            obj[i].yearOfDeath = currYear;
+        }
+        obj[i].age = obj[i].yearOfDeath - obj[i].yearOfBirth;
+    }
+
+    let tempObj;
+    obj.reduce((prevVal, currVal, currIndex) => {
+        if (currIndex < 1) tempObj = obj[currIndex];
+        if (tempObj.age < currVal.age) oldest = currIndex;
+        tempObj = currVal;
+    }, 0);
+
+    return obj[oldest];
 };
 
 // Do not edit below this line
